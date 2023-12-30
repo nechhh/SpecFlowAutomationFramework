51664b4950460be5eeb1bd897cc91a9752a9415e
nechhh <necirvanurek8@gmail.com>
2023-12-30 08:06:14 -0800

    API logic implemented

---
+++ Models/Response/SingleUserRes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowAutomationFramework.Models.Response
+{
+    
+
+
+    public class SingleUserRes
+    {
+        public Data data { get; set; }
+        public Support support { get; set; }
+    }
+
+  
+
+
+
+}
